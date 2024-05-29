a11583ff5a0e7ea0894ad25b5226dd1c16ae116d
Jan-Klaas Kesteloot <janklaas.kesteloot@skyline.be>
2024-05-29 11:07:50 +0200

    Add example Status Page

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ Examples include
 - [Amsterdam Internet Exchange](Amsterdam%20Internet%20Exchange) : scrapes data from the AMS-IX website for various locations, including Amsterdam, Bay Area, Caribbean, Chicago, Hong Kong, and Mumbai.
 - [Azure Data v2](Azure%20Data%20v2) : collects data on both active and deallocated Windows Server 2022 VMs in Azure.
 - [Coincap](Coincap) : fetches data from the CoinCap API, converting certain string values to floats, and sending the modified data to Data API.
+- [Status Page](Status%20Page) : retrieve status information from services using the Atlassian Status Page.
 
 # Meta Data (for Skyline Communications)
 
---
+++ Status Page/StatusPageComponents.py
@@ -0,0 +1,31 @@
+import requests
+
+def main():
+    # Define the service for which we are checking the status
+    # Example services using this API are GitHub, Dropbox, Discord, Vimeo
+    service = "GitHub Status"
+    
+    # Set the base URL of the status page API for the service
+    # Example API URLs for these services are:
+    # https://www.githubstatus.com, https://status.dropbox.com, https://discordstatus.com, https://www.vimeostatus.com/
+    urlapi = "https://www.githubstatus.com"
+
+    # Define header parameters for the request to the local API
+    header_params = {
+        "identifier": service,
+        "type": "Status Page",
+    }
+    
+    # Create a session object to manage and persist settings across requests
+    session = requests.Session()
+
+    # Send a GET request to the status API to get the current status of the components in JSON format
+    components = session.get(urlapi + "/api/v2/components.json")
+    
+    # Send a PUT request to the local Data API with the status data
+    # Include the header parameters for additional context
+    session.put("http://localhost:34567/api/data/parameters", json=components.json(), headers=header_params) 
+
+# Execute the main function when the script is run
+if __name__ == "__main__":
+    main()
